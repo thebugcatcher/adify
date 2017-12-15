4db960c17a644ea18de5695403843136bc1c7130
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-15 13:03:13 -0500

    Add initial shell script

---
+++ adify.sh
@@ -0,0 +1,28 @@
+### PRELUDE ###
+# This script is responsible for running all the commands required to setup an ubuntu computer
+# to be used by Adi Iyengar effectively. Hence, it adifies the computer.
+# This script doesn't not include the tools required to do "Work" related stuff
+
+# TODO: Make it runnable on other OS.
+# This Script is setup to run only on Ubuntu computers
+
+
+### REQUIREMENTS ###
+# - Internet Connection
+# - Admin Privilleges of the computer being adified
+# - Adi (for credentials)
+
+
+### PREFERRED TOOLS ###
+
+# Chromium
+sudo apt-get update
+sudo apt-get install -y chromium-browser
+
+# Git
+sudo apt update
+sudo apt install -y git
+
+# WGET
+sudo apt install wget
+
