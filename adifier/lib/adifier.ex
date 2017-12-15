d0f5b9a9b9fb7ab22a902af4898d4d0adfd7d325
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-15 13:04:30 -0500

    Add base elixir app adifier

---
+++ adifier/lib/adifier.ex
@@ -0,0 +1,18 @@
+defmodule Adifier do
+  @moduledoc """
+  Documentation for Adifier.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Adifier.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
