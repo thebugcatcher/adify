71be0ae0f6bdfc0894105a558c4b83e1ae198e59
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-15 22:05:15 -0500

    Add command opts to package manager

--- adifier/lib/package_manager.ex
+++ adifier/lib/package_manager.ex
@@ -3,13 +3,18 @@ defmodule Adifier.PackageManager do
   """
 
   @needsudo ~w(apt apt-get yum)
+  @cmdopts [into: IO.stream(:stdio, :line)]
 
   def package_managers(:ubuntu), do: ~w(apt apt-get)
   def package_managers(:centos), do: ~w(yum)
   def package_managers(:mac), do: ~w(brew)
 
-  def update_cmd(pm), do: "#{invoke_cmd(pm)} update"
+  def update_cmd(pm), do: invoke_cmd(pm, with: "update")
 
-  def invoke_cmd(pm) when pm in @needsudo, do: "sudo #{pm}"
-  def invoke_cmd(pm), do: "#{pm}"
+  def invoke_cmd(pm, with: str) when pm in @needsudo do
+    System.cmd("sudo", [pm | String.split(str)], @cmdopts)
+  end
+  def invoke_cmd(pm, with: str) do
+    System.cmd(pm, String.split(str), @cmdopts)
+  end
 end
