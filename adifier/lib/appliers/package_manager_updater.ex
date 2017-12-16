541e05d21d1a02ab1585f5d356d1749a0138db22
Adi Iyengar <aditya7iyengar@gmail.com>
2017-12-16 13:36:52 -0500

    Add PackageManager Updater

---
+++ adifier/lib/appliers/package_manager_updater.ex
@@ -0,0 +1,14 @@
+defmodule Adifier.Applier.PackageManagerUpdater do
+  @moduledoc """
+  """
+
+  @behaviour Adifier.Applier
+
+  import Adifier.PackageManager
+
+  def run(os) do
+    os
+    |> package_managers()
+    |> Enum.map(&update_cmd/1)
+  end
+end
