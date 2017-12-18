c84a98318612db2b1078b3b44b1e744711318494
Adi <aditya7iyengar@gmail.com>
2017-12-17 22:31:31 -0500

    Update docs

--- adify.sh
+++ adify.sh
@@ -1,6 +1,6 @@
 ### PRELUDE ###
 # This script is responsible for running all the commands required to setup an ubuntu computer
-# to be used by Adi Iyengar effectively. Hence, it adifies the computer.
+# for me to work effectively. Hence, it adifies the computer.
 # This script doesn't not include the tools required to do "Work" related stuff
 
 # TODO: Make it runnable on other OS.
@@ -33,7 +33,7 @@ if [ ! -d "$HOME/dot-adify" ]; then
   Adifying for the first time....
 
   This script is responsible for running all the commands required to setup an ubuntu computer
-  to be used by Adi Iyengar effectively. Hence, it adifies the computer.
+  for me to work effectively. Hence, it adifies the computer.
   This script doesn't not include the tools required to do 'work' elated stuff by default
   ==========================================================
   """
