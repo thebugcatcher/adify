f1822f8b5d7185b7959592a6546eda89401b96b3
Adi Iyengar <aditya7iyengar@gmail.com>
2018-07-08 13:21:26 -0400

    Change ubuntu docker to default

--- adifier/lib/adifier/tool/docker.ex
+++ adifier/lib/adifier/tool/docker.ex
@@ -7,33 +7,33 @@ defmodule Adifier.Tool.Docker do
 
   use Adifier.Tool, name: "docker"
 
-  @impl true
-  def install_cmd(:ubuntu) do
-    """
-    sudo sh -c "
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-		apt-get -y update
-		apt-cache policy docker-ce
-		apt-get install -y docker-ce
-		systemctl status docker
-    "
-    """
-  end
-
-  @impl true
-  def install_cmd(:pop_os) do
-    """
-    sudo sh -c "
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-		add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-		apt-get -y update
-		apt-cache policy docker-ce
-		apt-get install -y docker-ce
-		systemctl status docker
-    "
-    """
-  end
+  # @impl true
+  # def install_cmd(:ubuntu) do
+  #   """
+  #   sudo sh -c "
+  #   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	# 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	# 	apt-get -y update
+	# 	apt-cache policy docker-ce
+	# 	apt-get install -y docker-ce
+	# 	systemctl status docker
+  #   "
+  #   """
+  # end
+  #
+  # @impl true
+  # def install_cmd(:pop_os) do
+  #   """
+  #   sudo sh -c "
+  #   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+	# 	add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+	# 	apt-get -y update
+	# 	apt-cache policy docker-ce
+	# 	apt-get install -y docker-ce
+	# 	systemctl status docker
+  #   "
+  #   """
+  # end
 
   @impl true
   def install_cmd(os), do: super(os)
